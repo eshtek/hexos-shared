4a15f0de235b899323f2b7802df1d3a425906fda
Jerod Fritz <accounts@centogram.com>
2024-05-07 18:10:59 -0400

    Ability to add and configure local accounts that are then passed on to the finish server step

--- eshtek/common.ts
+++ eshtek/common.ts
@@ -15,6 +15,30 @@ export const servernameFormat = (hostname: string): boolean => {
     return !/\s/.test(hostname) && regex.test(hostname);
 };
 
+/*
+Username Format
+Length between 3 and 8 characters.
+Allowed characters: letters (both uppercase and lowercase), numbers, underscores, and hyphens.
+Cannot start or end with an underscore or hyphen.
+No consecutive underscores or hyphens.
+Must start with a letter
+*/
+export const usernameFormat = (username: string): boolean => {
+    const regex = /^[a-zA-Z][a-zA-Z0-9_-]{2,7}$/;
+    return regex.test(username);
+};
+/*
+Password format:
+Length between 8 and 30 characters.
+At least one uppercase letter, one lowercase letter, one number, and one special character.
+Allowed special characters: !@#$%^&*()-_=+[]{}|;:,.<>?`~/\
+No spaces allowed.
+*/
+export const passwordFormat = (password: string): boolean => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~\\-]).{8,30}$/;
+    return !/\s/.test(password) && regex.test(password);
+};
+
 export const capitalize = (s: string) => {
     return s.charAt(0).toUpperCase() + s.slice(1);
 };
