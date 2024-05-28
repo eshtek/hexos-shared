f7d8d810677f4fa36221300923a02bc202a1d024
Jerod Fritz <accounts@centogram.com>
2024-05-28 16:53:07 -0400

    DDP Calls made type compliant prior to integrating with a shared ddp-client class b/w front and back

--- enums/api-message-type.enum.ts
+++ enums/api-message-type.enum.ts
@@ -1,21 +1,24 @@
 export enum IncomingApiMessageType {
-  Changed = 'changed',
-  Added = 'added',
-  Removed = 'removed',
-  Result = 'result',
-  Connected = 'connected',
-  Pong = 'pong',
-  Method = 'method',
-  Ready = 'ready',
-  NoSub = 'nosub',
+    Changed = 'changed',
+    Added = 'added',
+    Removed = 'removed',
+    Result = 'result',
+    Connected = 'connected',
+    Pong = 'pong',
+    Method = 'method',
+    Ready = 'ready',
+    NoSub = 'nosub',
 
-  // Special type added on the frontend
-  Discard = 'discard',
+    // Special type added on the frontend
+    Discard = 'discard',
+
+    // Added by eshtek
+    Updated = 'updated',
 }
 
 export enum OutgoingApiMessageType {
-  Connect = 'connect',
-  UnSub = 'unsub',
-  Sub = 'sub',
-  Ping = 'ping',
+    Connect = 'connect',
+    UnSub = 'unsub',
+    Sub = 'sub',
+    Ping = 'ping',
 }
--- enums/on-off.enum.ts
+++ enums/on-off.enum.ts
@@ -1,11 +1,11 @@
-import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
+//import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 
 export enum OnOff {
-  On = 'ON',
-  Off = 'OFF',
+    On = 'ON',
+    Off = 'OFF',
 }
 
-export const onOffLabels = new Map<OnOff, string>([
-  [OnOff.On, T('On')],
-  [OnOff.Off, T('Off')],
-]);
+/*export const onOffLabels = new Map<OnOff, string>([
+    [OnOff.On, T('On')],
+    [OnOff.Off, T('Off')],
+]);*/
