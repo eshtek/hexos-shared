8a9fdb24cccdf35b1cb0e667408ce1241529a1cd
Jerod Fritz <accounts@centogram.com>
2024-06-02 23:38:28 -0400

    start template system for status blocks. work on circle indicator with brand color for secondary circle.  unsubscribe from events when components are unmounted

--- eshtek/common.ts
+++ eshtek/common.ts
@@ -42,6 +42,7 @@ export function isPrivateIP(ip: string) {
     const parts = ip.split('.');
     const result =
         parts[0] === '10' ||
+        parts[0] === '127' ||
         (parts[0] === '172' && parseInt(parts[1], 10) >= 16 && parseInt(parts[1], 10) <= 31) ||
         (parts[0] === '192' && parts[1] === '168');
     console.log('isPrivateIP', { ip, result });
--- interfaces/api/api-event-directory.interface.ts
+++ interfaces/api/api-event-directory.interface.ts
@@ -1,7 +1,11 @@
 import { FailoverStatus } from '@shared/enums/failover-status.enum';
 import { Alert } from '@shared/interfaces/alert.interface';
 import { ChartRelease, ChartReleaseStats } from '@shared/interfaces/chart-release.interface';
-import { PullContainerImageResponse, PullContainerImageParams, ContainerImage } from '@shared/interfaces/container-image.interface';
+import {
+    PullContainerImageResponse,
+    PullContainerImageParams,
+    ContainerImage,
+} from '@shared/interfaces/container-image.interface';
 import { DirectoryServicesState } from '@shared/interfaces/directory-services-state.interface';
 import { FailoverDisabledReasonEvent } from '@shared/interfaces/failover-disabled-reasons.interface';
 import { Group } from '@shared/interfaces/group.interface';
@@ -17,27 +21,29 @@ import { TrueCommandConfig } from '@shared/interfaces/true-command-config.interf
 import { User } from '@shared/interfaces/user.interface';
 import { VirtualMachine } from '@shared/interfaces/virtual-machine.interface';
 import { ZfsSnapshot } from '@shared/interfaces/zfs-snapshot.interface';
+import { SystemHealth } from '../system-health.interface';
 
 export interface ApiEventDirectory {
-  'alert.list': { response: Alert };
-  'chart.release.query': { response: ChartRelease };
-  'chart.release.statistics': { response: { id: string; stats: ChartReleaseStats } };
-  'core.get_jobs': { response: Job };
-  'directoryservices.status': { response: DirectoryServicesState };
-  'failover.status': { response: { status: FailoverStatus } };
-  'failover.disabled.reasons': { response: FailoverDisabledReasonEvent };
-  'service.query': { response: Service };
-  'truecommand.config': { response: TrueCommandConfig };
-  'vm.query': { response: VirtualMachine };
-  'zfs.snapshot.query': { response: ZfsSnapshot };
-  'zfs.pool.scan': { response: PoolScan };
-  'user.query': { response: User };
-  'container.image.pull': { response: Job<PullContainerImageResponse, PullContainerImageParams> };
-  'disk.query': { response: Disk };
-  'pool.query': { response: Pool };
-  'group.query': { response: Group };
-  'container.image.query': { response: ContainerImage };
-  'reporting.realtime': { response: ReportingRealtimeUpdate };
-  'smart.test.progress': { response: SmartTestProgressUpdate };
-  'kubernetes.state': { response: KubernetesStatusData };
+    'alert.list': { response: Alert };
+    'chart.release.query': { response: ChartRelease };
+    'chart.release.statistics': { response: { id: string; stats: ChartReleaseStats } };
+    'core.get_jobs': { response: Job };
+    'directoryservices.status': { response: DirectoryServicesState };
+    'failover.status': { response: { status: FailoverStatus } };
+    'failover.disabled.reasons': { response: FailoverDisabledReasonEvent };
+    'service.query': { response: Service };
+    'truecommand.config': { response: TrueCommandConfig };
+    'vm.query': { response: VirtualMachine };
+    'zfs.snapshot.query': { response: ZfsSnapshot };
+    'zfs.pool.scan': { response: PoolScan };
+    'user.query': { response: User };
+    'container.image.pull': { response: Job<PullContainerImageResponse, PullContainerImageParams> };
+    'disk.query': { response: Disk };
+    'pool.query': { response: Pool };
+    'group.query': { response: Group };
+    'container.image.query': { response: ContainerImage };
+    'reporting.realtime': { response: ReportingRealtimeUpdate };
+    'smart.test.progress': { response: SmartTestProgressUpdate };
+    'kubernetes.state': { response: KubernetesStatusData };
+    'system.health': { response: SystemHealth };
 }
