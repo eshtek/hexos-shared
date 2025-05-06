c6661f9e453cadf6d40833853463c2adb8e17684
Garrett <garrettjcox@gmail.com>
2025-05-06 11:09:05 -0500

    Add schema check for CI

---
+++ ts-to-zod.config.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        name: "vms",
+        input: "eshtek/vms.ts",
+        output: "eshtek/vms-schema.ts",
+    },
+    {
+        name: "server",
+        input: "eshtek/server.ts",
+        output: "eshtek/server-schema.ts",
+    },
+    {
+        name: "user",
+        input: "eshtek/user.ts",
+        output: "eshtek/user-schema.ts",
+    },
+];
