dd029fc02a2c31da9b36aab50fb61d5ae37fe077
Eric Schultz <eric@startuperic.com>
2025-05-16 11:05:11 -0500

    added GlobalErrors.AUTH_NOT_ADMIN

--- eshtek/errors.ts
+++ eshtek/errors.ts
@@ -1,6 +1,7 @@
 export enum GlobalErrors {
     AUTH_MISSING_2FA = '2FA code not provided',
     AUTH_MISSING_CREDENTIALS = 'Email or password not provided',
+    AUTH_NOT_ADMIN = 'User is not an administrator',
     AUTH_INVALID_CREDENTIALS = 'Invalid credentials',
     GENERIC_SERVER_ERROR = 'Server error, please try again later.',
 }
