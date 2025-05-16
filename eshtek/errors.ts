6d2bbe2dee0e278d2f528928596864836e15a4b0
Eric Schultz <eric@startuperic.com>
2025-05-16 11:33:51 -0500

    GlobalErrorCode revamp

--- eshtek/errors.ts
+++ eshtek/errors.ts
@@ -1,7 +1,7 @@
-export enum GlobalErrors {
-    AUTH_MISSING_2FA = '2FA code not provided',
-    AUTH_MISSING_CREDENTIALS = 'Email or password not provided',
-    AUTH_NOT_ADMIN = 'User is not an administrator',
-    AUTH_INVALID_CREDENTIALS = 'Invalid credentials',
-    GENERIC_SERVER_ERROR = 'Server error, please try again later.',
+export enum GlobalErrorCode {
+    AUTH_MISSING_2FA = 'AUTH_MISSING_2FA',
+    AUTH_MISSING_CREDENTIALS = 'AUTH_MISSING_CREDENTIALS',
+    AUTH_NOT_ADMIN = 'AUTH_NOT_ADMIN',
+    AUTH_INVALID_CREDENTIALS = 'AUTH_INVALID_CREDENTIALS',
+    GENERIC_SERVER_ERROR = 'GENERIC_SERVER_ERROR',
 }
--- eshtek/routes.ts
+++ eshtek/routes.ts
@@ -1,4 +1,4 @@
-import { GlobalErrors } from './errors';
+import { GlobalErrorCode } from './errors';
 import type { LocationPreferenceId } from './preferences';
 import type { ServerPool, ServerPoolNew, ServerUser, ServerUserType } from './server';
 import type { VMBasics, VMSettings } from './vms';
@@ -10,7 +10,7 @@ export type ResponseSuccess<T> = {
 
 export type ResponseError<T> = {
     success: false;
-    error: string | GlobalErrors;
+    error: string | GlobalErrorCode;
     data?: T;
 };
 export type Response<T> = ResponseSuccess<T> | ResponseError<T>;
