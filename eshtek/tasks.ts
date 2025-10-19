a13fd2477fb75708bc74e13b46e2b9b4b0ee0f39
Jerod Fritz <jerodfritz@gmail.com>
2025-10-19 11:26:47 -0400

    rename to PREFERENCE_LOCATION_PATH_MIGRATION

--- eshtek/tasks.ts
+++ eshtek/tasks.ts
@@ -47,7 +47,7 @@ export enum HexTaskType {
   APP_UNINSTALL = 'APP_UNINSTALL',
   APP_UPGRADE = 'APP_UPGRADE',
   APP_UPDATE = 'APP_UPDATE',
-  LOCATION_PREFERENCE_UPDATE = 'LOCATION_PREFERENCE_UPDATE',
+  PREFERENCE_LOCATION_PATH_MIGRATION = 'PREFERENCE_LOCATION_PATH_MIGRATION',
   POOLS_DELETE_ALL = 'POOLS_DELETE_ALL',
   DRIVE_REPLACE = 'DRIVE_REPLACE',
 }
@@ -126,7 +126,7 @@ export type HexTaskDataMap = {
   [HexTaskType.APP_UNINSTALL]: { hostId: string; data: { appId: string; error?: string }; parentTaskId?: string; error?: string };
   [HexTaskType.APP_UPGRADE]: { hostId: string; data: { appId: string; fromVersion?: string; toVersion?: string; error?: string }; parentTaskId?: string; };
   [HexTaskType.APP_UPDATE]: { hostId: string; data: { appId: string; reason?: 'location_preference_change'; locationPreferenceId?: string; error?: string }; parentTaskId?: string; };
-  [HexTaskType.LOCATION_PREFERENCE_UPDATE]: { hostId: string; data: { locationPreferenceId: string; oldPath: string; newPath: string; error?: string }; parentTaskId?: never; };
+  [HexTaskType.PREFERENCE_LOCATION_PATH_MIGRATION]: { hostId: string; data: { locationPreferenceId: string; oldPath: string; newPath: string; error?: string }; parentTaskId?: never; };
   [HexTaskType.DRIVE_REPLACE]: { hostId: string; data: { poolId: number; devname: string; newDevname: string, label: string, disk: string; error?: string }; parentTaskId?: string };
 };
 
@@ -156,6 +156,6 @@ export const HexTaskSettings: {
   [HexTaskType.APP_UNINSTALL]: { canHaveMultiple: true, predictedSecondsToComplete: 60 },
   [HexTaskType.APP_UPGRADE]: { canHaveMultiple: true, predictedSecondsToComplete: 90 },
   [HexTaskType.APP_UPDATE]: { canHaveMultiple: true, predictedSecondsToComplete: 300 },
-  [HexTaskType.LOCATION_PREFERENCE_UPDATE]: { canHaveMultiple: false, predictedSecondsToComplete: 1200 },
+  [HexTaskType.PREFERENCE_LOCATION_PATH_MIGRATION]: { canHaveMultiple: false, predictedSecondsToComplete: 1200 },
   [HexTaskType.DRIVE_REPLACE]: { canHaveMultiple: true, predictedSecondsToComplete: 120 },
 };
