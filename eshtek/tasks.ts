cdac32b5c369fcac771bed7d2eff55ad61a02653
Jerod Fritz <jerodfritz@gmail.com>
2025-10-19 09:56:57 -0400

    APP_UPDATE task added

--- eshtek/tasks.ts
+++ eshtek/tasks.ts
@@ -46,6 +46,7 @@ export enum HexTaskType {
   APP_INSTALL = 'APP_INSTALL',
   APP_UNINSTALL = 'APP_UNINSTALL',
   APP_UPGRADE = 'APP_UPGRADE',
+  APP_UPDATE = 'APP_UPDATE',
   POOLS_DELETE_ALL = 'POOLS_DELETE_ALL',
   DRIVE_REPLACE = 'DRIVE_REPLACE',
 }
@@ -122,6 +123,7 @@ export type HexTaskDataMap = {
   [HexTaskType.APP_INSTALL]: { hostId: string; data: { appId: string; train?: string; installType?: 'standard' | 'custom'; error?: string }; parentTaskId?: string; };
   [HexTaskType.APP_UNINSTALL]: { hostId: string; data: { appId: string; error?: string }; parentTaskId?: string; error?: string };
   [HexTaskType.APP_UPGRADE]: { hostId: string; data: { appId: string; fromVersion?: string; toVersion?: string; error?: string }; parentTaskId?: string; };
+  [HexTaskType.APP_UPDATE]: { hostId: string; data: { appId: string; error?: string }; parentTaskId?: string; };
   [HexTaskType.DRIVE_REPLACE]: { hostId: string; data: { poolId: number; devname: string; newDevname: string, label: string, disk: string; error?: string }; parentTaskId?: string };
 };
 
@@ -150,5 +152,6 @@ export const HexTaskSettings: {
   [HexTaskType.APP_INSTALL]: { canHaveMultiple: true, predictedSecondsToComplete: 300 },
   [HexTaskType.APP_UNINSTALL]: { canHaveMultiple: true, predictedSecondsToComplete: 60 },
   [HexTaskType.APP_UPGRADE]: { canHaveMultiple: true, predictedSecondsToComplete: 90 },
+  [HexTaskType.APP_UPDATE]: { canHaveMultiple: true, predictedSecondsToComplete: 300 },
   [HexTaskType.DRIVE_REPLACE]: { canHaveMultiple: true, predictedSecondsToComplete: 120 },
 };
